e35a767c94b3640f5f5137e6ea8861bc8a2dca2f
qoentz <englund.jonatan@gmail.com>
2025-02-25 03:47:02 +0100

    htmx fixes

--- static/js/main.js
+++ static/js/main.js
@@ -18,6 +18,27 @@ document.addEventListener('htmx:afterSwap', (event) => {
     }
 });
 
+document.addEventListener("htmx:historyRestore", (event) => {
+    console.log("htmx:historyRestore fired");
+
+    // Remove letter animation classes only from the logo text
+    document.querySelectorAll(".logo-text-wrapper span.animate-letter-slide-in")
+        .forEach(el => el.classList.remove("animate-letter-slide-in"));
+
+    // Remove the fade-slide animation from the container (but not the ball image)
+    document.querySelectorAll(".logo-wrapper.animate-fade-slide-in")
+        .forEach(el => el.classList.remove("animate-fade-slide-in"));
+});
+
+
+
+
+
+
+
+
+
+
 
 
 
